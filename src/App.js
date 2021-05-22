55061b8663fb1d970aa887be27ee73b3d06b2a1f
Gustavo Seabra <devguga87@gmail.com>
2021-05-22 01:11:27 -0300

    feature: app search and list components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
-function App() {
+const App = () => {
+  const stories = [
+    {
+      title:'React',
+      url:'https://reactjs.org/',
+      author:'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0
+    },
+    {
+      title:'Redux',
+      url:'https://redux.js.org/',
+      author:'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1
+    },
+  ];
+
+  const [searchTerm, setSearchTerm] = useState('')
+
+  function handleSearch(event){
+    setSearchTerm(event.target.value)
+  }
+
+  const filteredStories = stories.filter(story => story.title.toLowerCase().includes(searchTerm.toLowerCase()))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>My Hacker Stories</h1>
+        <Search onSearch={handleSearch}/>
+      <hr/>
+      <div>
+        <List list={filteredStories}/>
+      </div>
     </div>
   );
+}
+
+const Search = ({onSearch}) => {
+  return(
+    <div>
+      <label htmlFor='search'>Search:</label>
+      <input id='search' type='text' onChange={onSearch}></input>   
+    </div>
+  )
+}
+
+const List = ({list}) => {
+  return (
+    <div>
+      {list.map(item => {
+        return(
+          <div key={item.objectID}>
+          <a href={item.url} target='__blank'>{item.title}</a>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+      </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default App;
